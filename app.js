59ec8fa35d6a3e911d321b19d58b6a317d0c728c
Dmitry K <69163014+webdev163@users.noreply.github.com>
2022-06-30 16:30:49 +0400

    feat: add mongodb support

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = config.get('port') || 5000;
 
-app.listen(PORT, () => {
-  console.log(`App has been started on port ${PORT}`)
-})
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'));
+    app.listen(PORT, () => {
+      console.log(`App has been started on port ${PORT}`);
+    })
+  } catch (e) {
+    console.log('Server error', e.message);
+    process.exit(1);
+  }
+}
+
+start();
+
