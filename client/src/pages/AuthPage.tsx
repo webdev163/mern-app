c6e99101b7c1e248f98987ae1d0e7759bc6885d9
Dmitry K <69163014+webdev163@users.noreply.github.com>
2022-07-02 17:51:58 +0400

    feat: add auth page layout

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -2,8 +2,29 @@ import React, {FC} from 'react';
 
 export const AuthPage: FC = () => {
   return (
-    <div>
-      <h1>Auth Page</h1>
+    <div className="row">
+      <div className="col s6 offset-s3">
+          <h1>Link shortener</h1>
+          <div className="card grey lighten-3">
+              <div className="card-content">
+                  <span className="card-title">Authorization</span>
+                  <div className="wrapper">
+                      <div className="input-field">
+                          <input id="email" type="text" className="validate" name="email"></input>
+                          <label htmlFor="email">Email</label>
+                      </div>
+                      <div className="input-field">
+                          <input id="password" type="password" className="validate" name="password"></input>
+                          <label htmlFor="password">Password</label>
+                      </div>
+                  </div>
+              </div>
+              <div className="card-action">
+                <button className="btn blue darken-2">Login</button>
+                <button className="btn blue darken-2">Register</button>
+              </div>
+          </div>
+      </div>
     </div>
   )
 }
