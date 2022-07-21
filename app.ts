188034173c522fa2b5f8586f37a870f3db95263e
Dmitry K <69163014+webdev163@users.noreply.github.com>
2022-07-21 14:49:50 +0400

    feat: prepare for deploy

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import config from './config';
 import mongoose from 'mongoose';
+import path from 'path';
 
 const app: Express = express();
 const PORT: number = config.port || 5000;
@@ -13,6 +14,13 @@ app.use(express.json(options));
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'));
 app.use('/to', require('./routes/redirect.routes'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 async function start() {
   try {
